2dbd5406cf05402906f85cdd1c33ea2b8dd03107
K_KarthiKN_3 <karthikeyan@admindroid.com>
2024-01-24 23:18:50 +0530

    deployment code added

--- Backend/app/server.js
+++ Backend/app/server.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const errorHanler = require("./middlewares/errorhandler");
 const generateToken = require("./util/generateToken");
 const authHandler = require("./middlewares/authHandler");
+const path = require("path");
  
 
 const app = express();
@@ -124,6 +125,26 @@ app.post('/api/updatepProfile', async(req, res)=>{
         })
     }
 })
+
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve("..");
+console.log(__dirname1)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/Frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "Frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 
 // app.use(errorHanler)
